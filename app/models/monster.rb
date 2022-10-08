549c5f419e106479715b0a43a19c928d42803692
soyezequiel <alcayaga147147@gmail.com>
2022-10-08 14:53:19 +0000

    se configuro las credenciales de la api, y se intento agregar las validaciones pero falla en el response de la clase number_validator

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,3 +1,13 @@
 class Monster < ApplicationRecord
   validates :name, :birthdate, presence: true
+  validate :numero_de_telefono_tiene_que_ser_valido
+
+  def numero_de_telefono_tiene_que_ser_valido
+    if NumberValidator.new(phone).valid?
+      return
+    else 
+      error.add(:phone, :invalid)
+    end
+
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_04_000934) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_08_142358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_04_000934) do
     t.datetime "updated_at", null: false
     t.string "phone"
     t.date "birthdate"
+  end
+
+  create_table "number_validators", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tweets", force: :cascade do |t|
