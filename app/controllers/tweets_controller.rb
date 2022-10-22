d479efacdc27c3f690264b37bdab9ec0c3ee118b
soyezequiel <alcayaga147147@gmail.com>
2022-10-22 17:33:10 -0300

    Se arreglo el error que no permitia cargar el tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,20 +4,23 @@ def index
     @tweets = Tweet.order(:created_at)
   end
 
-  def show
+def show
     @tweet = Tweet.find(params[:id])
   end
+def new
+    @tweetNuevo=Tweet.new
+end
 
-  def create
-    @tweet=Tweet.new(tweet_params)
-    if  @tweet.save
-      redirect_to @tweet, notice: "todo legal"
-
+def create
+    @tweetNuevo=Tweet.new(tweet_params)
+    if  (@tweetNuevo.save)
+      redirect_to @tweetNuevo, notice: "todo legal"
     else
+      puts "error"
       render :new
     end
-  end
-  def tweet_params
-    params.require(:tweet).permit(:content)
+end
+def tweet_params
+    params.require(:tweet).permit(:content, :monster_id)
   end
 end
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,11 +1,12 @@
 class Monster < ApplicationRecord
   validates :name, :birthdate, presence: true
   validate :numero_de_telefono_tiene_que_ser_valido
+  has_many :tweet
 
   def numero_de_telefono_tiene_que_ser_valido
     if Phone.new(phone).valid?
       return
-    else 
+    else
       errors.add(:phone, :invalid, message: "pepito")
     end
 
